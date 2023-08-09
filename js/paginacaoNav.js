dd5f851bdb430a1ccea725597b826111519c5cfe
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-08-09 15:03:14 -0300

    navegation bar created

--- js/paginacaoNav.js
+++ js/paginacaoNav.js
@@ -1,27 +1,42 @@
 import { conectaApi } from "./conectaApi.js"
 import { listarPosts} from "./listarPosts.js"
+const totalPages = await conectaApi.pegaTotalPages()
+var paginaAtual = 0
+
 async function criarBarraPages(){
-    const totalPages = await conectaApi.pegaTotalPages()
-    console.log(totalPages)
     const ul = document.getElementById('paginacao__lista')
-    for (let i = 1; i <=totalPages; i++) {
-        const li = document.createElement('li')
-
-        const button = document.createElement('button')
-        button.addEventListener('click', (event => {
-            window.scrollTo({
-                top:0,
-                behavior:"smooth"
-            })
-            listarPosts.buscarPostsEMontarPosts(i-1)
-        }))
-        button.innerHTML = i
-        button.classList.add('paginacao__button')
-        li.classList.add('paginacao__item')
-        li.appendChild(button)
-        ul.appendChild(li)
-    }
-    
+    ul.innerHTML = ""
+    if(paginaAtual!=0)
+        navegacao("<")
+    if(paginaAtual!=totalPages)
+        navegacao(">")  
+   
 }
-
 criarBarraPages()
+
+function navegacao(irOuVir){
+    const ul = document.getElementById('paginacao__lista')
+    const li = document.createElement('li')
+    li.classList.add('paginacao__item')
+    const btn = document.createElement('button')
+    btn.classList.add('paginacao__button')
+    btn.innerHTML = irOuVir
+    btn.addEventListener('click', (event)=>{
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        })
+        if(event.target.textContent === '<'){
+            listarPosts.buscarPostsEMontarPosts(paginaAtual-1)
+            paginaAtual = paginaAtual-1
+        }
+        else{
+            listarPosts.buscarPostsEMontarPosts(paginaAtual+1)
+            paginaAtual = paginaAtual + 1
+        }
+        criarBarraPages()
+        console.log(paginaAtual)
+    })
+    li.appendChild(btn)
+    ul.appendChild(li)
+}
